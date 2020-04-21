5b3dfcf7f0339d9df21b9b280fdfe1d75731df14
meadowstream <jimmyrare@gmail.com>
2020-04-21 11:48:07 +0200

    Add project files.

---
+++ MeadowRPSTrainer/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+	float rock_frequency;
+	float paper_frequency;
+	float scissors_frequency;
+} strategy_t;
+
+enum Actions { ROCK, PAPER, SCISSORS };
+const int num_actions = SCISSORS;
+
+int main() {
+
+	// Setup
+
+	// Train
+
+	// Get solution
+
+	return 0;
+}
